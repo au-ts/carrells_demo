384102b151d978363f31348b650793652e45101d
Terry Bai <tianyi.bai@unsw.edu.au>
2026-09-09 16:00:41 +1000

    fix scripts
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 cd ./dep/dynamic-microkit && \
 nix develop --command bash -c "python build_sdk.py --skip-tar --boards=x86_64_generic_vtx --sel4=../sel4 --configs=smp-debug" && \
+cd ../../ && \
 pip install ./dep/microkit_sdf_gen
