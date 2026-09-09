2bae913f49a47e7f36ae75738d0fd0e4688072e7
Terry Bai <tianyi.bai@unsw.edu.au>
2026-09-09 14:56:17 +1000

    update dependencies
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ fi
 
 target_machine="$1"
 
-# rm -rf build && \
+rm -rf build && \
 make all -j$(nproc) BUILD_DIR=build MICROKIT_BOARD=x86_64_generic_vtx X86_BOARD=${target_machine} MICROKIT_CONFIG=smp-debug MICROKIT_SDK=/Users/terrybai/ts/microkit/release/microkit-sdk-2.3.0-dev
 /Users/terrybai/tmp/machine_queue/mq.sh run -s ${target_machine} -f ./build/sel4.elf -f ./build/loader.img -c "fnjkeqhtreqfgkjadfg"
