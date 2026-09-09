bcec5ef50fa328e9922a4f315d00589779c2561e
Terry Bai <tianyi.bai@unsw.edu.au>
2026-09-09 16:09:59 +1000

    fix scripts
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ fi
 target_machine="$1"
 
 rm -rf build && \
-nix develop ./dep/libvmm --command bash -c "make all -j$(nproc) BUILD_DIR=build MICROKIT_BOARD=x86_64_generic_vtx X86_BOARD=${target_machine} MICROKIT_CONFIG=smp-debug MICROKIT_SDK=./dep/dynamic-microkit/release/microkit-sdk-2.3.0-dev"
+nix develop ./dep/libvmm --command bash -c "pip install ./dep/microkit_sdf_gen && make all -j$(nproc) BUILD_DIR=build MICROKIT_BOARD=x86_64_generic_vtx X86_BOARD=${target_machine} MICROKIT_CONFIG=smp-debug MICROKIT_SDK=./dep/dynamic-microkit/release/microkit-sdk-2.3.0-dev"
 ./dep/machine_queue/mq.sh run -s ${target_machine} -f ./build/sel4.elf -f ./build/loader.img -c "fnjkeqhtreqfgkjadfg"
--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@
 
 cd ./dep/dynamic-microkit && \
 nix develop --command bash -c "python build_sdk.py --skip-tar --boards=x86_64_generic_vtx --sel4=../sel4 --configs=smp-debug" && \
-cd ../../ && \
-pip install ./dep/microkit_sdf_gen
