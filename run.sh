5b95274a1598332ea82645ffba95b1a5e82c7a18
Terry Bai <tianyi.bai@unsw.edu.au>
2026-09-09 15:19:17 +1000

    update scripts
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ target_machine="$1"
 
 rm -rf build && \
 make all -j$(nproc) BUILD_DIR=build MICROKIT_BOARD=x86_64_generic_vtx X86_BOARD=${target_machine} MICROKIT_CONFIG=smp-debug MICROKIT_SDK=/Users/terrybai/ts/microkit/release/microkit-sdk-2.3.0-dev
-/Users/terrybai/tmp/machine_queue/mq.sh run -s ${target_machine} -f ./build/sel4.elf -f ./build/loader.img -c "fnjkeqhtreqfgkjadfg"
+./dep/machine_queue/mq.sh run -s ${target_machine} -f ./build/sel4.elf -f ./build/loader.img -c "fnjkeqhtreqfgkjadfg"
